4ebf47ab26d0b569343f2260f68d8eb700bc53fb
Vivitougnou <76539149+Vivitougnou@users.noreply.github.com>
2021-02-01 22:19:48 +0100

    Delete invite.js

--- commandes/invite.js
+++
@@ -1,5 +0,0 @@
-exports.run = async (client, message) => {
-
-    message.channel.send(`${client.emotes.success} - Voici le lien du **Serveur Support de Musique.xyz** : https://discord.gg/nk6MpNQzuv ! Voici le lien d'invitation de **Musique.xyz** : https://discord.com/api/oauth2/authorize?client_id=805876550410829835&permissions=36718848&scope=bot`)
-
-};
